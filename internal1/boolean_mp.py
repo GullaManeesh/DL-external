3ad64c8f23c4dc58748d3d7eb2535d61abf6a4fd
Gulla Maneesh <gullamanish2045@gmail.com>
2026-05-27 20:30:04 +0530

    Update boolean_mp.py

--- internal1/boolean_mp.py
+++ internal1/boolean_mp.py
@@ -19,3 +19,43 @@ for x in inputs:
   print(f"{x}:{mp(x,1)}")
 
 
+
+#mp 
+
+import numpy as np
+
+def step(z):
+  return 1 if z>=0 else 0
+
+def mp(x,w,b):
+  z=np.dot(x,w)+b
+  return step(z)
+
+input = np.array([[0,0],[0,1],[1,0],[1,1]])
+
+#and
+for x in input:
+  print(f"{x}:{mp(x,[1,1],-1.5)}")
+
+
+#or
+for x in input:
+  print(f"{x}:{mp(x,[1,1],-0.5)}")
+
+
+#nor
+for x in input:
+  print(f"{x}:{mp(x,[-1,-1],0.5)}")
+
+
+#nand
+for x in input:
+  print(f"{x}:{mp(x,[-1,-1],1.5)}")
+
+
+# NOT
+for x in [[0],[1]]:
+    print(f"{x}:{mp(x,[-1],0.5)}")
+
+
+
